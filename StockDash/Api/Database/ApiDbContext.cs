a5a8ad5c79f80462ec06e75f5b0891cb69ca33f2
Di Wang <di.wang@outlook.co.nz>
2023-04-01 22:48:13 +1300

    Initial Setup for EF

--- StockDash/Api/Database/ApiDbContext.cs
+++ StockDash/Api/Database/ApiDbContext.cs
@@ -6,5 +6,16 @@ public class ApiDbContext : DbContext
     {
  
     }
-    public DbSet<SharesiesInstrument> Students { get; set; }
+    public DbSet<SharesiesInstrument> SharesiesInstrument { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(t => t.GetProperties()).Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+        {
+            property.SetPrecision(18);
+            property.SetScale(6);
+        }
+
+    }
 }
